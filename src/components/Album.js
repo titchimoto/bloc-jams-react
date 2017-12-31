aaede31e1e028f54d79159994534f1e09121c320
Richard Deane <titch@hotmail.co.uk>
2017-12-31 14:49:23 +0000

    Assignment 3 Routing: Complete

---
+++ src/components/Album.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Album extends Component {
+  render() {
+    return (
+      <section className="album">
+      Album Info will go here.
+      </section>
+    );
+  }
+}
+
+export default Album;
