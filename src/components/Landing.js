46657194acb6bea342b69f2f08761ebdd4e9af79
Richard Deane <titch@hotmail.co.uk>
2017-12-31 14:25:58 +0000

    Checkpoint 3 Routing: Complete

---
+++ src/components/Landing.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Landing extends Component {
+  render() {
+    return (
+      <section className="landing">
+        Landing page will go here
+      </section>
+    );
+  }
+}
+
+export default Landing;
---
+++ src/components/Library.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Library extends Component {
+  render() {
+    return (
+      <section className="library">
+        Library will go here
+      </section>
+    );
+  }
+}
+
+export default Library;
