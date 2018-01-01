08022f8ecdcd5b90a5d33bdadbdc4537796c6574
Richard Deane <titch@hotmail.co.uk>
2018-01-01 20:07:06 +0000

    Checkpoint 7 Audio: Complete

--- src/components/Album.js
+++ src/components/Album.js
@@ -11,9 +11,40 @@ class Album extends Component {
     });
 
     this.state = {
-      album: album
+      album: album,
+      currentSong: album.songs[0],
+      isPlaying: false
     };
+
+    this.audioElement = document.createElement('audio');
+    this.audioElement.src = album.songs[0].audioSrc;
   }
+
+    play() {
+      this.audioElement.play();
+      this.setState({ isPlaying: true });
+    }
+
+    pause() {
+      this.audioElement.pause();
+      this.setState({ isPlaying: false });
+    }
+
+    setSong(song) {
+      this.audioElement.src = song.audioSrc;
+      this.setState({ currentSong: song });
+    }
+
+    handleSongClick(song) {
+      const isSameSong = this.state.currentSong === song;
+      if (this.state.isPlaying && isSameSong) {
+        this.pause();
+      } else {
+        if (!isSameSong) { this.setSong(song); }
+        this.play();
+      }
+    }
+
   render() {
     return (
       <section className="album">
@@ -33,9 +64,8 @@ class Album extends Component {
             <col id="song-duration-column" />
           </colgroup>
           <tbody>
-          {
-          this.state.album.songs.map( (songs, index) =>
-          <tr>
+          { this.state.album.songs.map( (song, index) =>
+          <tr className="song" key={index} onClick={() => this.handleSongClick(song)} >
            <td className="song-number">
              <button>
                <span className="song-number-column">{index + 1}</span>
@@ -43,8 +73,8 @@ class Album extends Component {
                <span className="ion-pause"></span>
             </button>
            </td>
-           <td className="song-title">{songs.title}</td>
-           <td className="song-duration">{songs.duration}</td>
+           <td className="song-title">{song.title}</td>
+           <td className="song-duration">{song.duration}</td>
           </tr>
             )
           }
