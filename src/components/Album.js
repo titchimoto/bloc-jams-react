e4c0c9ed303fcf51815aff3bd23ee0046a840e3f
Richard Deane <titch@hotmail.co.uk>
2018-01-03 10:17:34 +0000

    Assignment 8 Buttons: Complete

--- src/components/Album.js
+++ src/components/Album.js
@@ -54,6 +54,14 @@ class Album extends Component {
       this.play(newSong);
     }
 
+    handleNextClick() {
+      const currentSongIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+      const newSongIndex = Math.min(this.state.album.songs.length - 1, currentSongIndex + 1);
+      const newSong = this.state.album.songs[newSongIndex];
+      this.setSong(newSong);
+      this.play(newSong);
+    }
+
   render() {
     return (
       <section className="album">
@@ -95,6 +103,7 @@ class Album extends Component {
           currentSong={this.state.currentSong}
           handleSongClick={ () => this.handleSongClick(this.state.currentSong)}
           handlePrevClick={ () => this.handlePrevClick()}
+          handleNextClick={ () => this.handleNextClick()}
         />
       </section>
     );
